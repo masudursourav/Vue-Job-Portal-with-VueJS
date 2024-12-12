64802c2cac4547cbd86d145d6326040b41a03805
Masudur Rahman Sourav <66933835+masudursourav@users.noreply.github.com>
2024-12-12 23:40:25 +0600

    job listing component added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HeroTab from './components/HeroTab.vue'
 import HomeCards from './components/HomeCards.vue'
+import JobListing from './components/JobListing.vue'
 import NavBar from './components/NavBar.vue'
 </script>
 <template>
   <NavBar />
   <HeroTab />
   <HomeCards />
+  <JobListing limit="3" showBtn="true" />
 </template>
